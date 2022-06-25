1642c05b4ca183ea1f0b42a914324eb89ac9006e
Mustafa Kurt <mustafakurt4362@hotmail.com>
2022-06-25 12:58:34 +0300

    Kodlamaio

---
+++ DictionaryKodLamaio/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryKodLamaio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kutuphane<int, string> isimler = new Kutuphane<int, string>();
+
+            isimler.Ekle(0, "Mustafa");
+            isimler.Ekle(1, "Elif");
+        
+            //eklemekte sıkıntı yok ama for veya foreach ile ekrana yazdıramıyorum.
+        }
+    }
+
+    class Kutuphane<a, b>
+    {
+        a[] a_;
+        b[] b_;
+
+
+        public Kutuphane()
+        {
+            a_ = new a[0];
+            b_ = new b[0];
+        }
+
+
+        public void Ekle(a key, b value)
+        {
+            a[] tempKey = a_;
+            b[] tempValue = b_;
+
+            a_ = new a[a_.Length + 1];
+            b_ = new b[b_.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                a_[i] = tempKey[i];
+                b_[i] = tempValue[i];
+
+            }
+
+            a_[a_.Length - 1] = key;
+            b_[b_.Length - 1] = value;
+        }
+    }
+}
---
+++ DictionaryKodLamaio/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("DictionaryKodLamaio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DictionaryKodLamaio")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("4f0de163-d822-44d0-8ae1-511bfc20f24c")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
